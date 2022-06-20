378489f39daf2a7084d8de4d47d305cde968f592
Andrianaaaa <38298216+Andrianaaaa@users.noreply.github.com>
2022-06-21 00:11:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Sequence.View;
+
+namespace Sequence
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var menu = new Menu();
+            menu.PrintCommands();
+        }
+    }
+}
